dce306e381215fab4712bbef76b8643bda7db0bb
Shiruyaka <tomasz.adam.skrzypczak@gmail.com>
2017-07-27 08:07:00 +0200

    Changing encoder

--- KompresjaDanych/KompresjaDanych/KompresjaDanych.srcs/sources_1/new/Buffor.vhd
+++ KompresjaDanych/KompresjaDanych/KompresjaDanych.srcs/sources_1/new/Buffor.vhd
@@ -39,26 +39,28 @@ entity Buffor is
            ready : out STD_LOGIC;
            clk : in STD_LOGIC;
            end_data : in STD_LOGIC;
-           end_state : in STD_LOGIC_VECTOR(7 downto 0);
-           
+          
            produce_symbol : out STD_LOGIC;
            
-           nbBits : in STD_LOGIC_VECTOR(3 downto 0); -- change from integer 
-           stream : out STD_LOGIC_VECTOR(7 downto 0) := "00000000";
+           nbBits : in STD_LOGIC_VECTOR(3 downto 0); 
+           stream : out STD_LOGIC_VECTOR(7 downto 0);
            x : in STD_LOGIC_VECTOR(7 downto 0)
           );
 end Buffor;
 
 architecture use_bits of Buffor is
 
-type state_type is (IDLE, IN_PROGRESS, CHECKING_BUFFOR, EMPTY_BUFF1, EMPTY_BUFF2, ADD_AMOUNT_DISJOINT_BYTES, OUT_END_STATE);
+type state_type is (IDLE, ENCODING, EMPTY_BUFF, OUT_AM_BYTES, OUT_STATE);
 signal current_state, next_state : state_type;
 
 signal buffor: std_logic_vector(0 to 31):= x"00000000";
+signal encoded_symbol, end_state: std_logic_vector(0 to 7) := x"00";
+signal action: std_logic_vector(0 to 2) := "000";
+signal buffor_fill, bits, disjointed_bytes: integer := 0;
 
 begin
     
-    state_machine: process(CLK)
+    state_machine: process(clk)
     begin
      if(rising_edge(clk)) then
         if(init = '1')then
@@ -68,112 +70,124 @@ begin
         end if;
       end if;
     end process;
-
-    main_process: process(current_state, start)
     
-    variable bits: std_logic_vector(7 downto 0);
-    variable bits_shifted: std_logic_vector(31 downto 0) := x"00000000";
-    variable counter: integer:= 0;
-    variable mask, disjoint_bytes: std_logic_vector(7 downto 0);
-    variable nbBitsInt: integer;
     
+    update_buffor: process(clk, action)
+    begin 
+        if(rising_edge(clk))then
+            
+            case action is
+            
+            when "001" =>
+                if(buffor_fill + bits >= 8) then
+                    stream <= encoded_symbol(0 to 7 - buffor_fill) & buffor(32 - buffor_fill to 31);
+                    produce_symbol <= '1';
+                    buffor <= (0 to 39 - buffor_fill - bits => '0') & encoded_symbol(8 - buffor_fill to bits - 1 );
+                    buffor_fill <= bits - 8 + buffor_fill;
+                else
+                    buffor <= buffor or ((23 - buffor_fill downto 0 => '0') & encoded_symbol & (buffor_fill - 1 downto 0 => '0'));
+                    buffor_fill <= buffor_fill + bits;
+                end if;
+            
+            when "010" =>
+                
+                
+                if(buffor_fill > 5) then 
+                    
+                    disjointed_bytes <= disjointed_bytes + 1;
+                    produce_symbol <= '1';
+                    
+                    stream  <= std_logic_vector(to_unsigned(5, 3)) & buffor(27 to 31);
+                    buffor_fill <= buffor_fill - 5;
+                    buffor <= (4 downto 0 => '0') & buffor(0 to 26);
+                
+                elsif(buffor_fill > 0) then
+                    
+                    disjointed_bytes <= disjointed_bytes + 1;
+                    produce_symbol <= '1';
+                    
+                    stream <= std_logic_vector(to_unsigned(buffor_fill, 3)) &
+                                              (4 - buffor_fill downto 0 => '0') &
+                                              buffor(31 downto 32 - buffor_fill); 
+                    buffor_fill <= 0;
+                    buffor <= x"00000000";
+                
+                else        
+                
+                    disjointed_bytes <= 0;
+                
+                end if;
+            
+            when "011" =>
+                
+                stream <= std_logic_vector(to_unsigned(disjointed_bytes, 8));
+                
+            when "100" =>
+                
+                stream <= end_state;
+                
+            when others =>
+                produce_symbol <= '0';            
+            
+            end case;
+            
+            
+            
+        else
+             produce_symbol <= '0';
+        end if;
+    end process;
+    
+    
+    encode: process(current_state, start)
+        
     begin
         next_state <= current_state;
         
-            case current_state is 
- 
-                when IDLE =>
-                    
-                    ready <= '0';
-                    produce_symbol <= '0';
-                                           
-                    if(start = '1')then
-                        next_state <= IN_PROGRESS;
-                    end if;
- 
-                when IN_PROGRESS =>
-                    nbBitsInt := to_integer(unsigned(nbBits));
-                                
-                    mask := (
-                             nbBitsInt - 2 downto 0 => '1',
-                             others => '0'
-                             );
-                             
-                    bits := x and mask;
-                    bits_shifted :=  (23 - counter downto 0 => '0') & bits & (counter - 1 downto 0 => '0');                                           
-                    
-                    buffor <= buffor or bits_shifted;
-                    counter := counter + nbBitsInt;
-                    
-                    next_state <= CHECKING_BUFFOR;
-             
-                when CHECKING_BUFFOR =>
-                
-                    if(counter >= 8) then
-                        
-                        stream <= buffor(24 to 31);
-                        produce_symbol  <= '1';
-                        buffor <= (0 to 7 => '0') & buffor(0 to 23);
-                                  
-                        counter := counter - 8;
-                
-                    end if;
-                    
-                    if(end_data = '1') then
-                       if(counter = 0) then
-                         disjoint_bytes := x"00";
-                         next_state <= ADD_AMOUNT_DISJOINT_BYTES;
-                       else 
-                           next_state <= EMPTY_BUFF1;
-                       end if;
-                              
-                    else
-                       next_state <= IDLE;               
-                    
-                    end if;
-                    
-               when EMPTY_BUFF1 =>
-               
-                   if(counter <= 5) then
-                        
-                        stream <= std_logic_vector(to_unsigned(counter, 3)) & (4 - counter downto 0 => '0') & buffor(31 downto 32 - counter); 
-                        disjoint_bytes := x"01";
-                        produce_symbol  <= '1';
-                        next_state <= OUT_END_STATE;
-                                      
-                   else
-                    
-                        stream <= std_logic_vector(to_unsigned(5, 3)) & buffor(27 to 31);
-                        disjoint_bytes := "00000010";
-                        buffor <= (4 downto 0 => '0') & buffor(0 to 26);
-                        counter := counter - 5;
-                        produce_symbol  <= '1';
-                        next_state <= EMPTY_BUFF2;
-                    
-                   end if;
-                  
-               when EMPTY_BUFF2 =>
-                    
-                    stream <= std_logic_vector(to_unsigned(counter, 3)) & (0 to 4 - counter => '0') & buffor(32 - counter to 31);
-                    produce_symbol  <= '1';
-                    next_state <= ADD_AMOUNT_DISJOINT_BYTES;
-               
-               when ADD_AMOUNT_DISJOINT_BYTES =>
-                    produce_symbol  <= '1';
-                    stream <= std_logic_vector(unsigned(disjoint_bytes));
-                    next_state <= OUT_END_STATE;
-               
-                
-               when OUT_END_STATE =>
-               
-                    stream <= end_state;
-                    ready <= '1';             
-                    
-                    next_state <= IDLE;
-               
-      end case;
+        case current_state is
+        
+        when IDLE =>
+        
+            ready <= '0';           
+            action <= "111";
+                                       
+            if(start = '1')then
+                next_state <= ENCODING;
+            end if;
             
+        when ENCODING =>
+            
+            bits <= to_integer(unsigned(nbBits)); 
+            encoded_symbol <= x(to_integer(unsigned(nbBits)) - 1 downto 0) & 
+                              (7 - to_integer(unsigned(nbBits)) downto 0 => '0'); 
+            action <= "001";        
+            
+            if(end_data = '0')then
+                next_state <= IDLE;
+            else
+                action <= "010";
+                end_state <= x;
+                next_state <= EMPTY_BUFF;
+            end if;                                                     
+            
+        when EMPTY_BUFF =>
+         
+            if(buffor_fill = 0) then
+                next_state <= OUT_AM_BYTES;
+                action <= "011";
+            end if;
+        
+        when OUT_AM_BYTES =>
+            
+            action <= "100";
+            next_state <= OUT_STATE;
+            
+        when OUT_STATE =>
+        
+            action <= "111";
+            next_state <= IDLE;
+                
+        end case;   
     end process;
-
-
+    
 end use_bits;
